79b7818e5cedc9d8b11795baeeda1fef5cc52822
FriedPotatoe <lloydlim1@gmail.com>
2021-01-21 13:12:13 +0800

    Lab16

---
+++ 13_Lloyd_Lab16/Assets/Scrips/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    static int Scoree;
+    public GameObject ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+            Scoree++;
+            Text ScoreText = GameObject.Find("Score").GetComponent<Text>();
+            ScoreText.text = "Score : "+Scoree;
+        }
+    }
+}
+    
---
+++ 13_Lloyd_Lab16/Assets/Scrips/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float speedrotate;
+    public Rigidbody RB;
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * speedrotate, 0));
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -(Time.deltaTime * speedrotate), 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+        }
+    }
+}
+
